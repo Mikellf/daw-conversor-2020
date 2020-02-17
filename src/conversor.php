1122eef5865376a10c2b5267bc2aa70d0ae60c36
Mik <mikellf2000@hotmail.com>
2020-02-17 19:24:23 +0100

    Primer commit, declaración de clases Conversor y ConversorTest

---
+++ src/conversor.php
@@ -0,0 +1,41 @@
+<?php 
+
+/**
+ * Doc comment short description: examen de despliegue
+ * 
+ * PHP version 7.4
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category  Conversor
+ * @package   Conversor
+ * @author    Mikellf <mikellf2000@hotmail.com>
+ * @copyright 2020 Arista Web Studio
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://pear.php.net/package/PackageName
+ */
+
+
+/**
+ * Short description for class
+ *
+ * @category  Conversor
+ * @package   Conversor
+ * @author    Mikellf <mikellf2000@hotmail.com>
+ * @copyright 2020 Arista Web Studio
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://pear.php.net/package/PackageName
+ */
+
+class Conversor
+{
+    
+
+}
+
+
+?>
---
+++ tests/conversorTest.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once('./src/conversor.php');
+
+use PHPUnit\Framework\TestCase;
+
+class ConversorTest extends TestCase {
+ 
+}
+
+?>
